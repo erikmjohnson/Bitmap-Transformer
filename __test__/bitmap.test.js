9badbdf824e783fdbe13f852a2ab35653e776766
erikmjohnson <erikarisma98@gmail.com>
2019-03-02 12:05:48 -0800

    80.6%

--- __test__/bitmap.test.js
+++ __test__/bitmap.test.js
@@ -2,6 +2,7 @@
 
 const readFile = require('../index');
 const Transform = require('../lib/transform');
+const Transformer = require('../lib/transformCheck');
 
 describe ('testing Bitmap functionality', () => {
   test('testing if bmp', () => {
@@ -10,37 +11,37 @@ describe ('testing Bitmap functionality', () => {
   test('testing allblack transform', () => {
     const testArray = [1,2,3,5];
     const test = new Transform(testArray);
-    test.allBlack();
+    Transformer('allblack', test);
     expect(testArray).toEqual([0,0,0,0]);
   });
   test('testing negative transform', () => {
     const testArray = [1,2,3,5];
     const test = new Transform(testArray);
-    test.negative();
+    Transformer('negative', test);
     expect(testArray).toEqual([3,2,1,0]);
   });
-  test('testing neon transform', () => {
+  test('testing pinkhighlights transform', () => {
     const testArray = [1,2,3,0];
     const test = new Transform(testArray);
-    test.pinkHighlights();
+    Transformer('pinkhighlights', test);
     expect(testArray).toEqual([1,2,3,255]);
   });
   test('testing allWhite transform', () => {
     const testArray = [1,2,3,0];
     const test = new Transform(testArray);
-    test.allWhite();
+    Transformer('allwhite', test);
     expect(testArray).toEqual([255,255,255,255]);
   });
   test('testing neon1 transform', () => {
     const testArray = [1,2,3,0];
     const test = new Transform(testArray);
-    test.neon1();
+    Transformer('neon1', test);
     expect(testArray).toEqual([0,191,255,0,191,255]);
   });
   test('testing neon2 transform', () => {
     const testArray = [1,2,3,0];
     const test = new Transform(testArray);
-    test.neon2();
+    Transformer('neon2', test);
     expect(testArray).toEqual([255,0,191,0]);
   });
   // further tests on if class/objects work
